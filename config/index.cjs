ba12a78f11b287a5e95270c13a8668648818d231
Luboly <92169955+Luboly@users.noreply.github.com>
2024-10-29 22:02:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o5GiC6Hiz71RywlGDSc55uY9Apaw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'B8Od9QWnin5YhvE3MV_2OON4Sk7tk-GE8M-4LJF1OzM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-22',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'lhjZJR9mtPlzN6XyU01vM7Bd5rZcg7t5Eha-HSF7Usw',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o5GiC6Hiz71RywlGDSc55uY9Apaw',
     }
   ],
 
